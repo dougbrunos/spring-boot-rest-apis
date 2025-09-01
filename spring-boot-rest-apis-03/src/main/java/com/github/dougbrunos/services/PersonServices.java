5217ab202da4e8cf69a323153d2218d3c8f899f1
Douglas Bruno <dougbrunos@gmail.com>
2025-09-01 10:00:20 -0300

    Verbos HTTP GET, POST, PUT and DELETE

--- spring-boot-rest-apis-03/src/main/java/com/github/dougbrunos/services/PersonServices.java
+++ spring-boot-rest-apis-03/src/main/java/com/github/dougbrunos/services/PersonServices.java
@@ -1,5 +1,7 @@
 package com.github.dougbrunos.services;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -14,6 +16,17 @@ public class PersonServices {
 
     private Logger logger = Logger.getLogger(PersonServices.class.getName());
 
+    public List<Person> findAll() {
+        logger.info("Finding all People!");
+
+        List<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 8; i++) {
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+        return persons;
+    }
+
     public Person findById(String id) {
         logger.info("Finding one Person!");
 
@@ -22,6 +35,32 @@ public class PersonServices {
         person.setFirstName("Douglas");
         person.setLastName("Souza");
         person.setAddress("Cascavel - Parana - Brazil");
+        person.setGender("Male");
+        return person;
+    }
+
+    public Person create(Person person) {
+        logger.info("Creating one Person!");
+
+        return person;
+    }
+
+    public Person update(Person person) {
+        logger.info("Updating one Person!");
+
+        return person;
+    }
+
+    public void delete(String id) {
+        logger.info("Deleting one Person!");
+    }
+
+    private Person mockPerson(int i) {
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Firstname " + i);
+        person.setLastName("Lastname " + i);
+        person.setAddress("Some Adress in Brazil");
         person.setGender("Male");
         return person;
     }
