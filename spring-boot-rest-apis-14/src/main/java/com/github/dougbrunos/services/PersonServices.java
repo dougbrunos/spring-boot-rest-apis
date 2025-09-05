e768f32b07ea9557ad5bf8827e2835ed41f7b498
Douglas Bruno <dougbrunos@gmail.com>
2025-09-05 12:21:11 -0300

    Integrations Tests w/ TestContainers e Rest Assured

---
+++ spring-boot-rest-apis-14/src/main/java/com/github/dougbrunos/services/PersonServices.java
@@ -0,0 +1,93 @@
+package com.github.dougbrunos.services;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+import org.springframework.stereotype.Service;
+
+import static com.github.dougbrunos.mapper.ObjectMapper.parseListObjects;
+import static com.github.dougbrunos.mapper.ObjectMapper.parseObject;
+
+import com.github.dougbrunos.controller.PersonController;
+import com.github.dougbrunos.controller.TestLogController;
+import com.github.dougbrunos.data.dto.PersonDTO;
+import com.github.dougbrunos.exception.ResourceNotFoundException;
+import com.github.dougbrunos.model.Person;
+import com.github.dougbrunos.repository.PersonRepository;
+
+@Service
+public class PersonServices {
+
+    private Logger logger = LoggerFactory.getLogger(TestLogController.class.getName());
+
+    @Autowired
+    PersonRepository repository;
+
+    public List<PersonDTO> findAll() {
+        logger.info("Finding all People!");
+
+        var persons = parseListObjects(repository.findAll(), PersonDTO.class);
+        persons.forEach(p -> addHateoasLinks(p));
+        return persons;
+    }
+
+    public PersonDTO findById(Long id) {
+        logger.info("Finding one Person!");
+
+        var entity = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+
+        var dto = parseObject(entity, PersonDTO.class);
+        addHateoasLinks(dto);
+        return dto;
+    }
+
+    public PersonDTO create(PersonDTO person) {
+        logger.info("Creating one Person!");
+
+        var entity = parseObject(person, Person.class);
+
+        var dto = parseObject(repository.save(entity), PersonDTO.class);
+        addHateoasLinks(dto);
+        return dto;
+    }
+
+    public PersonDTO update(PersonDTO person) {
+        logger.info("Updating one Person!");
+
+        Person entity = repository.findById(person.getId())
+                .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+
+        entity.setFirstName(person.getFirstName());
+        entity.setLastName(person.getLastName());
+        entity.setAddress(person.getAddress());
+        entity.setGender(person.getGender());
+
+        var dto = parseObject(repository.save(entity), PersonDTO.class);
+        addHateoasLinks(dto);
+        return dto;
+    }
+
+    public void delete(Long id) {
+        logger.info("Deleting one Person!");
+
+        Person entity = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+
+        repository.delete(entity);
+    }
+
+    private void addHateoasLinks(PersonDTO dto) {
+        dto.add(linkTo(methodOn(PersonController.class).findById(dto.getId())).withSelfRel().withType("GET"));
+        dto.add(linkTo(methodOn(PersonController.class).findAll()).withRel("findAll").withType("GET"));
+        dto.add(linkTo(methodOn(PersonController.class).create(dto)).withRel("create").withType("POST"));
+        dto.add(linkTo(methodOn(PersonController.class).update(dto)).withRel("update").withType("PUT"));
+        dto.add(linkTo(methodOn(PersonController.class).delete(dto.getId())).withRel("delete").withType("DELETE"));
+    }
+
+}
---
+++ spring-boot-rest-apis-14/src/test/java/com/github/dougbrunos/config/TestConfigs.java
@@ -0,0 +1,7 @@
+package com.github.dougbrunos.config;
+
+public interface TestConfigs {
+
+    int SERVER_PORT = 8888;
+
+}
